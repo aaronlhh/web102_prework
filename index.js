0120020efec54a7aa3c3bac94b2639b8d272cb02
aaron <aaron.lhh0401@gmail.com>
2023-12-27 16:48:07 -0800

    everything until customization

--- index.js
+++ index.js
@@ -181,7 +181,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [first, second, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+let firstGameName = document.createElement("p");
+firstGameName.innerHTML = first["name"];
+firstGameContainer.appendChild(firstGameName);
 
 // do the same for the runner up item
+let secondGameName = document.createElement("p");
+secondGameName.innerHTML = second["name"];
+secondGameContainer.appendChild(secondGameName);
